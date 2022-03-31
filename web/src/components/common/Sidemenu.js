d49635ff67bb193fdeaf35eaa94e87f74e10b65d
Leopard0505 <k.9975hyo5@gmail.com>
2022-03-31 19:38:48 +0900

    サイドメニューを修正

--- web/src/components/common/Sidemenu.js
+++ web/src/components/common/Sidemenu.js
@@ -11,22 +11,6 @@ function Sidemenu() {
                 <li className="is-padding-xxs">
                     <a href="/books/create" className="text is-success">購読本（マスタ）登録</a>
                 </li>
-                <hr className="mbook-border"></hr>
-                <li className="is-padding-xxs">
-                    <a href="/" className="text">サイドメニュー3</a>
-                </li>
-                <li className="is-padding-xxs">
-                    <a href="/" className="text">サイドメニュー4</a>
-                </li>
-                <li className="is-padding-xxs">
-                    <a href="/" className="text">サイドメニュー5</a>
-                </li>
-                <li className="is-padding-xxs">
-                    <a href="/" className="text">サイドメニュー6</a>
-                </li>
-                <li className="is-padding-xxs">
-                    <a href="/" className="text">サイドメニュー7</a>
-                </li>
             </ul>
         </nav>
     )
