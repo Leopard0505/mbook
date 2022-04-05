ea944b3b0befd04f746851505c9634f1be397f72
Leopard0505 <k.9975hyo5@gmail.com>
2022-04-05 21:20:40 +0900

    リンクタグを変更

--- web/src/components/common/Sidemenu.tsx
+++ web/src/components/common/Sidemenu.tsx
@@ -1,17 +1,18 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 const Sidemenu: React.VFC = () => {
     return (
         <nav>
             <ul className="is-padding-md">
                 <li className="is-padding-xxs">
-                    <a href="/" className="text is-success">購読本一覧</a>
+                    <Link to="/" className="text is-success">購読本一覧</Link>
                 </li>
                 <li className="is-padding-xxs">
-                    <a href="/books/register" className="text is-success">購読本登録</a>
+                    <Link to="/books/register" className="text is-success">購読本登録</Link>
                 </li>
                 <li className="is-padding-xxs">
-                    <a href="/books/create" className="text is-success">購読本（マスタ）登録</a>
+                    <Link to="/books/create" className="text is-success">購読本（マスタ）登録</Link>
                 </li>
             </ul>
         </nav>
